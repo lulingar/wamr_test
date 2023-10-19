47b6071e0b4dc866f55f11cbfebb1cbe0055a95f
Luis Linares <luis@midokura.com>
2023-10-19 11:49:46 +0200

    provide no_thread/threaded split for wasmtime

---
+++ example_wasmtime/c/main_threaded.c
@@ -0,0 +1,148 @@
+/*
+Example of instantiating a WebAssembly which uses WASI imports.
+Based on https://docs.wasmtime.dev/examples-c-wasi.html
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <wasm.h>
+#include <wasi.h>
+#include <wasmtime.h>
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+static void* wasm_runner(void *vp);
+static void exit_with_error(const char *message, wasmtime_error_t *error, wasm_trap_t *trap);
+
+struct wasm_args
+{
+    wasm_engine_t *engine;
+    wasmtime_store_t *store;
+    wasmtime_context_t *context;
+    wasmtime_linker_t *linker;
+};
+void spawn_wasm_instance(pthread_t* thread, struct wasm_args* args);
+
+int main(int argc, char *argv[])
+{
+    // Set up our context
+    wasm_config_t *config = wasm_config_new();
+    assert(config != NULL);
+    wasmtime_config_wasm_threads_set(config, true);
+
+    wasm_engine_t *engine = wasm_engine_new_with_config(config);
+    assert(engine != NULL);
+    wasmtime_store_t *store = wasmtime_store_new(engine, NULL, NULL);
+    assert(store != NULL);
+    wasmtime_context_t *context = wasmtime_store_context(store);
+
+    // Create a linker with WASI functions defined
+    wasmtime_linker_t *linker = wasmtime_linker_new(engine);
+    wasmtime_error_t *error = wasmtime_linker_define_wasi(linker);
+    if (error != NULL)
+        exit_with_error("failed to link wasi", error, NULL);
+
+    // Load our input file to parse it next
+    wasm_byte_vec_t wasm;
+    FILE *file = fopen(argv[1], "rb");
+    if (!file)
+    {
+        printf("> Error loading file!\n");
+        exit(1);
+    }
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = ftell(file);
+    wasm_byte_vec_new_uninitialized(&wasm, file_size);
+    fseek(file, 0L, SEEK_SET);
+    if (fread(wasm.data, file_size, 1, file) != 1)
+    {
+        printf("> Error loading module!\n");
+        exit(1);
+    }
+    fclose(file);
+
+    // Compile our modules
+    wasmtime_module_t *module = NULL;
+    error = wasmtime_module_new(engine, (uint8_t *)wasm.data, wasm.size, &module);
+    if (!module)
+        exit_with_error("failed to compile module", error, NULL);
+    wasm_byte_vec_delete(&wasm);
+
+    // Instantiate wasi
+    wasi_config_t *wasi_config = wasi_config_new();
+    assert(wasi_config);
+    wasi_config_inherit_argv(wasi_config);
+    wasi_config_inherit_env(wasi_config);
+    wasi_config_inherit_stdin(wasi_config);
+    wasi_config_inherit_stdout(wasi_config);
+    wasi_config_inherit_stderr(wasi_config);
+    wasm_trap_t *trap = NULL;
+    error = wasmtime_context_set_wasi(context, wasi_config);
+    if (error != NULL)
+        exit_with_error("failed to instantiate WASI", error, NULL);
+
+    // Instantiate the module
+    error = wasmtime_linker_module(linker, context, "", 0, module);
+    if (error != NULL)
+        exit_with_error("failed to instantiate module", error, NULL);
+
+    pthread_t t1, t2;
+    struct wasm_args args = {.engine = engine, .store = store, .context = context, .linker = linker};
+    spawn_wasm_instance(&t1, &args);
+    spawn_wasm_instance(&t2, &args);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    // Clean up after ourselves at this point
+    wasmtime_module_delete(module);
+    wasmtime_store_delete(store);
+    wasm_engine_delete(engine);
+    return 0;
+}
+
+static void* wasm_runner(void *vp)
+{
+    struct wasm_args *w = vp;
+
+    wasmtime_func_t func;
+    wasm_trap_t *trap = NULL;
+    wasmtime_error_t *error = wasmtime_linker_get_default(w->linker, w->context, "", 0, &func);
+    if (error != NULL)
+        exit_with_error("failed to locate default export for module", error, NULL);
+
+    error = wasmtime_func_call(w->context, &func, NULL, 0, NULL, 0, &trap);
+    if (error != NULL || trap != NULL)
+        exit_with_error("error calling default export", error, trap);
+}
+
+void spawn_wasm_instance(pthread_t* thread, struct wasm_args* args)
+{
+    int ret = pthread_create(thread, NULL, wasm_runner, args);
+    if (ret != 0)
+    {
+        fprintf(stderr, "pthread_create error: %d\n", ret);
+        exit(1);
+    }
+}
+
+static void exit_with_error(const char *message, wasmtime_error_t *error, wasm_trap_t *trap)
+{
+    fprintf(stderr, "error: %s\n", message);
+    wasm_byte_vec_t error_message;
+    if (error != NULL)
+    {
+        wasmtime_error_message(error, &error_message);
+        wasmtime_error_delete(error);
+    }
+    else
+    {
+        wasm_trap_message(trap, &error_message);
+        wasm_trap_delete(trap);
+    }
+    fprintf(stderr, "%.*s\n", (int)error_message.size, error_message.data);
+    wasm_byte_vec_delete(&error_message);
+    exit(1);
+}
